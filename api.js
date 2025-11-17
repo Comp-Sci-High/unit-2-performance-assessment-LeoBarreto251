7238230402dadde182133c812eb7e5b8b606e908
LeoBarreto251 <leandro.barreto26@compscihigh.org>
2025-11-17 20:19:46 +0000

    I'm working on only outputting the URL of the image

--- api.js
+++ api.js
@@ -5,3 +5,16 @@ const apiKey = process.env.apiKey2
 // run your code with node api.js
 // add your apiKey to .env if your API needs one, otherwise ignore
 // Follow the checklist on the Performance Assessment Google Doc
+
+
+async function getCatPic(request){
+const response = await fetch(request)
+const data = await response.json()
+console.log(data.url)
+return data.url
+}
+console.log("Welcome to the Cat API!")
+let limit = prompt("Type the amount of cat pics you want to see (1-10): ")
+
+let requestURL = `https://api.thecatapi.com/v1/images/search?limit=${limit}`
+getCatPic(requestURL)
