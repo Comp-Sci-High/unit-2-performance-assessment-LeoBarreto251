8082d6c858bce812bd23e39d7a8adc9c8571ebc4
LeoBarreto251 <leandro.barreto26@compscihigh.org>
2025-11-18 20:15:46 +0000

    COmpleted Cat API

--- api.js
+++ api.js
@@ -10,11 +10,11 @@ const apiKey = process.env.apiKey2
 async function getCatPic(request){
 const response = await fetch(request)
 const data = await response.json()
-console.log(data.url)
-return data.url
+console.log(data[0].url)
+return data
 }
 console.log("Welcome to the Cat API!")
 let limit = prompt("Type the amount of cat pics you want to see (1-10): ")
-
+console.log(`Fetching ${limit} cat image(s)...`)
 let requestURL = `https://api.thecatapi.com/v1/images/search?limit=${limit}`
 getCatPic(requestURL)
